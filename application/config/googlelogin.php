2114e540e0158fcc489d07b75fcc6bb1f9958ece
DanielMastori <danielmastori@gmail.com>
2025-11-18 01:14:34 -0300

    borrado de secret

--- application/config/googlelogin.php
+++ application/config/googlelogin.php
@@ -1,8 +1,3 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['google_client_id']     = '555266891698-5lnvr8tinm8lespamnj3cv2be3t6df5i.apps.googleusercontent.com';
-$config['google_client_secret'] = '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';
-
-//$config['google_redirect_uri']  = base_url('index/googleCallback'); 
-$config['google_redirect_uri']  = 'http://localhost/glosoft/index.php/Index_controller/googleCallback'; 
